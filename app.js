ba4b6a46a8b1ae7c1a4c045b31adf37e06618614
shreyash9999 <dongareshreyash20@gmail.com>
2023-07-07 15:27:50 +0530

    mongoose new version using then, cath v-7...

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const express = require("express");
 const app = express();
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {useNewUrlParser: true,useUnifiedTopology:true},(err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Connected db");
-    }
+// mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {useNewUrlParser: true,useUnifiedTopology:true},(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Connected db");
+//     }
+// }
+// );
+
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {useNewUrlParser: true,useUnifiedTopology:true},)
+    .then(()=>console.log("connect db success"))
+    .catch((err)=>{console.error(err);
 }
 );
 
