023e68c6d15e0af720a236632bd649785e159ecc
shreyash9999 <dongareshreyash20@gmail.com>
2023-07-07 15:22:28 +0530

    mongo with 6.10.0 version

---
+++ app.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {useNewUrlParser: true,useUnifiedTopology:true},(err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Connected db");
+    }
+}
+);
+
+const fruitSchema = new mongoose.Schema({
+    name:String,
+    rating:Number,
+    review:String
+});
+
+const Fruit = mongoose.model("Fruit",fruitSchema);
+
+const fruit  = new Fruit({
+    name: "Apple",
+    rating : 7,
+    review: "Hm pretty red apple"
+});
+
+fruit.save();
+
+
+app.get("/", function(req, res) {
+    res.send("oo its up!");
+});
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("server up on 3000");
+});
+
+// mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {useNewUrlParser: true});
+// //connection url
+
+
+// const fruitSchema = new mongoose.Schema({
+//     name:String,
+//     rating:Number,
+//     review:String
+// });
+
+// const Fruit = mongoose.model("Fruit",fruitSchema);
+
+// const fruit  = new Fruit({
+//     name: "Apple",
+//     rating : 7,
+//     review: "Hm pretty red apple"
+// });
+
+// fruit.save();
